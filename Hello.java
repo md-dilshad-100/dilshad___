8bc9e711266070c3037f3b083e609eba88f3e2f2
Dilshad <mohddilshad0473@gmail.com>
2024-03-04 22:30:03 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+package com.dilshad;
+
+public class Hello {
+public static void main(String[] args) {
+	System.out.println("dilshad");
+}
+
+}
---
+++ mohd.java
@@ -0,0 +1,12 @@
+package com.dilshad;
+
+public class mohd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     for(int i=1; i<=100; i++) {
+    	 System.out.println("Mohd Dilshad"+" "+i);
+     }
+	}
+
+}
